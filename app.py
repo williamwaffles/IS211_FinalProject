7d534a1d0dfdee16fe373fb341b96a01e75a801e
williamwaffles <41193361+williamarnaiz@users.noreply.github.com>
2021-12-21 21:25:47 -0500

    Added redirections in html files

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, redirect, request
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import sqlalchemy
 from datetime import datetime
+
+# for reference: https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/#a-minimal-application
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-db = SQLAlchemy(app)
+db = sqlalchemy(app)
+
 
 class Bloggr(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,15 +19,50 @@ class Bloggr(db.Model):
     def __repr__(self):
         return self.title
 
+
 db.create_all()
 db.session.commit()
 
 @app.route('/')
 @app.route('/home')
-@app.route('/posts')
 @app.route('/Bloggr')
-def welcome():
+def Welcome():
     return render_template('index.html')
+
+@app.route('/posts',  methods=['GET', 'POST'])
+def posts():
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['post']
+        post_author = request.form['author']
+        new_post = Bloggr(title=post_title, content=post_content, posted_by=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect('/posts')
+    else:
+        all_posts = Bloggr.query.order_by(Bloggr.posted_on).all()
+        return render_template('posts.html', posts=all_posts)
+
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    to_delete = Bloggr.query.get_or_404(id)
+    db.session.delete(to_delete)
+    db.session.commit()
+    return redirect('/posts')
+
+@app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    to_edit = Bloggr.query.get_or_404(id)
+    if request.method == 'POST':
+        to_edit.title = request.form['title']
+        to_edit.author = request.form['author']
+        to_edit.content = request.form['post']
+        db.session.commit()
+        return redirect('/posts')
+
+    else:
+        return render_template('edit.html', post=to_edit)
 
 @app.route('/posts/new', methods=['GET', 'POST'])
 def new_post():
@@ -39,22 +77,5 @@ def new_post():
     else:
         return render_template('new_post.html')
 
-@app.route('/posts',  methods=['GET', 'POST'])
-def posts():
-    if request.method == 'POST':
-        post_title = request.form['title']
-        post_content = request.form['post']
-        post_author = request.form['author']
-        new_post = Bloggr(title=post_title, content=post_content, posted_by=post_author)
-        db.session.add(new_post)
-        db.session.commit()
-        return redirect('/posts')
-    else:
-        all_posts = Bloggr.query.order_by(Bloggr.posted_on).all()
-        return render_template('posts.html', posts=all_posts)
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
